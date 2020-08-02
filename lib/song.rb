cc66ff51b0d2cdc9078fcec19c4e9d38d11a955c
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-02 02:19:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,6 +18,10 @@ class Song
   # self.create means song.create because self is the class Song
   # in this class method, we initialize a song and save it with the .save method. If we look at the .save class method, it's saying that the class itself (song in this case) should be saved to the @@all array.
   
-  
+  def self.new_by_name(song_name)
+    song = self.new 
+    song.name = song_name
+    song
+  end
   
 end   #Class End 
